575b09541f50c2a94d5b00eded03368ec45e2343
Neil Wilcoxson <neil.wilcoxson@gmail.com>
2016-11-18 19:47:29 -0600

    Driver Update
    Trees now run at a reasonable speed.

--- main.cpp
+++ main.cpp
@@ -21,17 +21,18 @@ int main(int argc, char *argv[])
 	SDL_Plotter g(WIN_RES_ROWS,WIN_RES_COLS);
 	//Background object for main level is created
 	Background forest;
+	forest.generate(BKG_IMG_NAME,NUM_BKG_FRAMES);
 
 	int frame = 0;	//Current background frame
 
     //Keep window open until quit state is reached (ESC)
     while (!g.getQuit()){
-    	forest.generate(BKG_IMG_NAME,NUM_BKG_FRAMES);
     	forest.draw(g,frame);
     	g.update();
     		if(g.kbhit()){
     	    	g.getKey();
     		}
+    	g.Sleep(100);
     	//Increment current frame number and keep within valid range
     	frame++;
     	frame %= NUM_BKG_FRAMES;
