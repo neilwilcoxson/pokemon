5aaddf665f6ba283c84085728d9aa09a481f53f8
Neil Wilcoxson <neil.wilcoxson@gmail.com>
2016-12-03 15:07:45 -0600

    Circle Progress

--- Circle.cpp
+++ Circle.cpp
@@ -8,3 +8,26 @@
 */
 
 #include "Circle.h"
+
+void Circle::generate(string fileName){
+    ifstream file(fileName.c_str());
+    file >> height >> width;
+    vector<vector<Pixel> >circle(height,vector<Pixel>(width));
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            file >> circle[i][j].R >>  circle[i][j].G >> circle[i][j].B;
+        }
+    }
+}
+
+void Circle::draw(int row, int col){
+
+}
+
+void Circle::move(direction dir){
+
+}
+void Circle::getPosition(int& row, int& col){
+
+}
--- Circle.h
+++ Circle.h
@@ -10,11 +10,23 @@
 #ifndef CIRCLE_H_
 #define CIRCLE_H_
 
+#include <vector>
+
+using namespace std;
+
+struct Pixel{
+    int R,G,B;
+};
+
+enum direction {UP,DOWN,LEFT,RIGHT};
+
 class Circle{
 	private:
-		int rowPos, colPos;
+		int rowPos, colPos, height, width;
 	public:
+	    void generate(string fileName);
 		void draw(int row, int col);
+		void move(direction dir)
 		void getPosition(int& row, int& col);
 };
 
