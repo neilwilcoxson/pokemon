11a93c0813919d3d96758633faac68113f6318dc
Neil Wilcoxson <neil.wilcoxson@gmail.com>
2016-12-03 10:16:30 -0600

    Updated Main and Pokemon Screens
    Minor changes, attempted sound playback, didn't work so commented out

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "BatteryMeter.h"
 #include "Pokemon.h"
 #include "Overlays.h"
+#include <ctime>
 
 using namespace std;
 
@@ -23,15 +24,28 @@ int main(int argc, char *argv[])
 	//Primary display plotter is created
 	SDL_Plotter g(WIN_RES_ROWS,WIN_RES_COLS);
 
+	//Keep track of time program started
+	int startTime = time(0);
+
 	Background loading;
 	loading.generate("loading");
 	loading.draw(g,0);
+	//g.initSound("PokemonTheme.wav");
+	//g.playSound("PokemonTheme.wav");
 	g.update();
 
-	//Vector of Pokemon Names
+	//Array of Pokemon Names
 	string pokemonNames[15] = {"Charmeleon","Diglet","Jigglypuff","Meowth",
                                 "Noctowl","Pikachu","Topegi","Totodile"};
     Pokemon pokemonObjects[15];
+
+    //Load Background for Start Screen
+    Background startScreen;
+    startScreen.generate("PokemonStartScreen");
+
+    //Load Background for instructions screen
+    Background instructionsScreen;
+    instructionsScreen.generate("PokemonInstructionsScreen");
 
 	//Background object for main level is created
 	Background city;
@@ -42,24 +56,55 @@ int main(int argc, char *argv[])
         pokemonObjects[i].generate(pokemonNames[i].c_str());
     }
 
+    //Create battery
 	Battery bat;
+	bool gameOver = false;
+
+	//Counter variable for current frame of game play
+	//Use %(number of frames in file) to select frame of each object
+	long long int frame = 0;
+
+	//Pause on loading screen if short to prevent flashing
+	if(time(0) - startTime < 2){
+		g.Sleep(1000);
+	}
+
+	//Draw start screen
+	startScreen.draw(g,0);
+	g.update();
+
+	//Wait until a key is pressed
+	while(!g.kbhit()){
+		g.Sleep(5);
+	}
+
+	//g.quitSound("PokemonTheme.wav");
+	g.update();
+
+	//Draw instructions screen
+	instructionsScreen.draw(g,0);
+	g.update();
+
+	//Wait until a key is pressed
+	while(!g.kbhit()){
+		g.Sleep(5);
+	}
+
 	bat.setBatteryLife(60);
 
-	long long int frame = 0;	//Current frame
-
     //Keep window open until quit state is reached (ESC)
-    while (!g.getQuit()){
+    while (!g.getQuit() && !gameOver){
     	city.draw(g,0);
-        bat.displayMeter(g,0,0);
-        pokemonObjects[1].draw(g,frame%pokemonObjects[1].getFrames(),0,0);
+        bat.displayMeter(g,5,5);
+        pokemonObjects[1].draw(g,frame%pokemonObjects[1].getFrames(),400,300);
     	g.update();
     		if(g.kbhit()){
     	    	g.getKey();
     		}
     	g.Sleep(100);
-    	pokemonObjects[1].erase(g,city,frame%city.getFrames(),bat);
-    	g.update();
-    	g.Sleep(100);
+    	//pokemonObjects[1].erase(g,city,frame%city.getFrames(),bat);
+    	//g.update();
+    	//g.Sleep(100);
     	//Increment current frame number
     	frame++;
     }
